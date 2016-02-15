efb089a351bd69b229a812761b8d526cab3a3912
Melanie Burger <melanie.brgr@gmail.com>
2016-02-15 18:33:42 -0500

    add GSAP and simple initial SVG animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $(function() {
-	//Make AJAX call to 23andme API
+	var $boy = $("#boy");
+
+	var Tl = new TimelineMax();
+	Tl
+		.set($boy, {autoAlpha: 0, transformOrigin: 'center center', scale: 0.5})
+		.to($boy, 0.8, {autoAlpha: 1, scale: 1}, '+=1');
 });
