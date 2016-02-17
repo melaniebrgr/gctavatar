b615de473b5ec57e35e29a861d29b920c5321866
Melanie Burger <melanie.brgr@gmail.com>
2016-02-16 22:57:44 -0500

    integrate with CodeKit

--- step-two.php
+++ step-two.php
@@ -7,7 +7,7 @@ $post_field_array = array(
   'client_secret' => '122f530d3904f0781b8f637d8aeefa3c',
   'grant_type'    => 'authorization_code',
   'code'          => $code,
-  'redirect_uri'  => 'http://localhost:8888/step-two.php',
+  'redirect_uri'  => 'http://melanies-macbook-pro.local:5757/step-two.php',
   'scope'         => 'basic genomes');
 
 // Encode the field values for HTTP.
