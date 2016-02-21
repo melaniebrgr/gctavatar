049ce96c7dae577bf3a6ba986d387b942662e721
Melanie Burger <melanie.brgr@gmail.com>
2016-02-21 18:32:06 -0500

    transfer cookie logic to app.js and link to index.html

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
+// Utility functions
 function log(m) {
 	console.log(m);
 }
 
 $(function() {
-var $avatar = $(".avatar");
-$avatar.height( $avatar.width() );
 
-//Animation	
-	var $boy = $("#boy");
+// If access_token is available in the cookie, skip authorization
+if ( Cookies.get('access_token') ) {
+	$(".getAuth").remove();
+	$(".description").append("<button class='getData'>Get Data</button>");
+	$(".getData").click(function() {
+		$.get( "/results.php", function(data) {
+			console.log(data);
+		} );
+	});
+}
 
-	var Tl = new TimelineMax();
-	Tl
-		.set($boy, {autoAlpha: 0, scale: 0.5})
-		.to($boy, 0.8, {autoAlpha: 1, scale: 1}, '+=1');
+// SVG Animation
+// var $avatar = $(".avatar");
+// $avatar.height( $avatar.width() );	
+// var $boy = $("#boy");
+// var Tl = new TimelineMax();
+// Tl
+// 	.set($boy, {autoAlpha: 0, scale: 0.5})
+// 	.to($boy, 0.8, {autoAlpha: 1, scale: 1}, '+=1');
 });
